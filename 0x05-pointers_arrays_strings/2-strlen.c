bf9e9408dd7dd36b598843ea1c7ca651e80950d8
Yonas <yonasleykun27@gmail.com>
2026-03-02 01:22:11 -0800

    tasks

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to check
+ *
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+	return (length);
+}
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of a string
+ * @str: string to process
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int i, start;
+
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		start = len / 2;
+	else
+		start = (len + 1) / 2;
+
+	for (i = start; i < len; i++)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
